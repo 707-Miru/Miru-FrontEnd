6003a071607c6652978b0f22578121101a6b6991
yoonkyu <newjeff@naver.com>
2022-08-04 14:53:28 +0900

    0804 YK Set PictureView

--- miru-frontend/src/components/PictureList.vue
+++ miru-frontend/src/components/PictureList.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <picture-list-item v-for="picture in pictures"
-    :key="picture.id"
+  <div id="columns">
+    <picture-list-item v-for="picture in p"
+    :key="picture.url"
     :picture="picture"
     >
     </picture-list-item>
@@ -25,7 +25,26 @@ export default {
 
   data () {
     return {
-
+      p:[
+        {
+          'url': require('@/assets/images/art.png')
+        },
+        // {
+        //   'url': require('@/assets/images/background.png')
+        // },
+        {
+          'url': require('@/assets/images/search.png')
+        },
+        {
+          'url': require('@/assets/images/season.png')
+        },
+        {
+          'url': require('@/assets/images/weather.png')
+        },
+        {
+          'url': require('@/assets/frame.png')
+        }
+      ]
     }
   },
 
@@ -44,5 +63,19 @@ export default {
 </script>
 
 <style>
-
+#columns {
+  margin: auto;
+  width: 90%;
+  column-count:3;
+  column-gap: 15px;
+}
+#columns img{
+  display: inline-block;
+  border:1px solid rgba(0,0,0,0.2);
+  margin:0;
+  margin-top: 15px;
+  padding:10px;
+  box-shadow: 2px 2px 5px rgba(0,0,0,0.5);;
+  width: 100%;
+}
 </style>
