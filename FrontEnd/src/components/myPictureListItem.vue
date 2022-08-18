fe5809e86b5c84d99afa40bafcb3ebf3189491ec
hyun722ls <hyun722ls@gmail.com>
2022-08-18 13:23:48 +0900

    0818 SH Modify myPictureList
    Former-commit-id: e69bea7ae6844b4e1bc584047fa627db1b2478c5

--- FrontEnd/src/components/myPictureListItem.vue
+++ FrontEnd/src/components/myPictureListItem.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-      <div>
-        <img class="mw-100 mh-100" :src="myPicture.url" :alt="myPicture.id">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg position-absolute top-0 end-0 me-3 mt-1" viewBox="0 0 16 16" @click="deleteData(myPicture.id)">
-          <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-        </svg>
-      </div>
+  <div class="myItem">
+    <div class="delete">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" @click="deleteData(myPicture.id)">
+        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+      </svg>
+    </div>
+    <img :src="myPicture.url" :alt="myPicture.id">
   </div>
 </template>
 
@@ -18,6 +18,19 @@ export default {
   props: {
     myPicture: Object
   },
+
+  data () {
+    return {
+      url : 'https://i7a707.p.ssafy.io/api/img/'
+    }
+  },
+
+  computed: {
+    filepath () {
+      return this.myPicture.filepath.split('/').reverse()[0]
+    }
+  },
+
   methods: {
     ...mapActions(['deletePicture']),
     deleteData(id) {
@@ -33,6 +46,35 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.draggable {
+  cursor: pointer;
+}
 
+.myItem {
+  position: relative;
+}
+
+.delete {
+  display: inline-block;
+  color: black;
+  border-radius: 2px;
+  transition: .2s;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: -15px;
+  padding: 10px 10px 8px 6px;
+  opacity: 0;
+  transition: .25s;
+  font-size: 14px;
+  text-align: right;
+}
+
+.myIteme:hover .delete {
+  opacity: 1;
+  top: 10px;
+  left: 22px;
+  right: 22px;
+}
 </style>
